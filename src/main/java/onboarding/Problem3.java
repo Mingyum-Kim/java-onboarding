dc3709ffb4c2e64c21737d65839ee541fc747043
MingyeomKim <mingyum119@naver.com>
2023-10-12 23:21:56 +0900

    feat(Problem2): DP 로직 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,19 @@
 package onboarding;
 
 public class Problem3 {
+
+
     public static int solution(int number) {
-        int answer = 0;
+        int[] dp = new int[10001];
+
+        for(int i = 1; i <= number; i++){
+            dp[i] = dp[i - 1] + count369OfNumber(i);
+        }
+        int answer = dp[number];
         return answer;
+    }
+
+    private static int count369OfNumber(int number) {
+
     }
 }
