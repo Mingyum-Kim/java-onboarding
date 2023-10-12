916062a6cccf013e456dc0fe14415d85bd323167
MingyeomKim <mingyum119@naver.com>
2023-10-12 22:23:46 +0900

    feat(Problem2): 문자열 중복 제거 로직 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -9,7 +9,22 @@ public class Problem2 {
         Stack<Character> stack = new Stack<>();
 
         while(!isCompleted) {
+            String temp = "";
+            for(int i = 0; i < cryptogram.length() - 1; i++){
+                if(!stack.empty() && stack.peek() == cryptogram.charAt(i)) {
+                    stack.clear();
+                } else if (cryptogram.charAt(i) != cryptogram.charAt(i + 1)){
+                    temp += cryptogram.charAt(i);
+                } else if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)){
+                    stack.push(cryptogram.charAt(i));
+                }
+            }
 
+            // 마지막 문자 검사
+            int last = cryptogram.length() - 1;
+            if(stack.empty() || stack.peek() != cryptogram.charAt(last)){
+                temp += cryptogram.charAt(last);
+            }
         }
 
         String answer = "answer";
