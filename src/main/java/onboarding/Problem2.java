e37bfe2cc8f48172470548bf13234904a3d20352
MingyeomKim <mingyum119@naver.com>
2023-10-12 22:20:34 +0900

    feat(Problem2): 문자열 검사 while loop 생성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,7 +1,17 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
+
+        boolean isCompleted = false;
+        Stack<Character> stack = new Stack<>();
+
+        while(!isCompleted) {
+
+        }
+
         String answer = "answer";
         return answer;
     }
