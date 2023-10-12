b2cf736ad24a6896a70a487bc3d3db8e4c66b184
MingyeomKim <mingyum119@naver.com>
2023-10-12 20:49:44 +0900

    feat(Problem1): 데이터 유효성 검사

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,35 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    private static final Integer EXCEPTION_NUMBER = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if(isNotIntegerList(pobi) || isNotIntegerList(crong)){
+            return EXCEPTION_NUMBER;
+        }
+
+        if(isOutOfRange(pobi) || isOutOfRange(crong)){
+            return EXCEPTION_NUMBER;
+        }
+
+        if(isNotTwoElements(pobi) || isNotTwoElements(crong)){
+            return EXCEPTION_NUMBER;
+        }
+
         int answer = 0;
         return answer;
+    }
+
+    private static boolean isNotIntegerList(List<Integer> pages){
+        return pages.get(0).getClass() != Integer.class && pages.get(1).getClass() != Integer.class;
+    }
+
+    private static boolean isOutOfRange(List<Integer> pages){
+        return (pages.get(0) < 1 || pages.get(0) > 400) || (pages.get(1) < 1 || pages.get(1) > 400);
+    }
+
+    private static boolean isNotTwoElements(List<Integer> pages){
+        return pages.size() != 2;
     }
 }
